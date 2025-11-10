ec42ad128a96bb7d6bd401369c403cbbac36bf47
alexDAW-IOC <alexioc852@gmail.com>
2025-11-10 18:14:25 +0000

    Configuración inical del proyecto SpringBoot y devcontainers

---
+++ src/main/java/com/equipo03/motorRecomendaciones/MotorRecomendacionesApplication.java
@@ -0,0 +1,13 @@
+package com.equipo03.motorRecomendaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorRecomendacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorRecomendacionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equipo03/motorRecomendaciones/MotorRecomendacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo03.motorRecomendaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorRecomendacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
